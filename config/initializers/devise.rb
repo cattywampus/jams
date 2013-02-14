d89af92bfdacbba10d5c58b6a36f923731aff1f8
Keith Walters <keith.walters@cattywamp.us>
2013-02-14 02:50:04 -0500

    Improved sign in screens

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -230,7 +230,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
   require "openid/store/filesystem"
-  config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
+  config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {access_type: 'online', approval_prompt: ''}
   config.omniauth :open_id, :store => OpenID::Store::Filesystem.new('/tmp'), require: 'omniauth-openid'
   
   # ==> Warden configuration
