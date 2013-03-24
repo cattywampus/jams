45f56000027945a37c02c78421885513295495cd
Keith Walters <keith.walters@cattywamp.us>
2013-03-24 18:48:38 -0400

    don't pass current_user name to paper_trail if not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def info_for_paper_trail
-    { ip: request.remote_ip, whodunnit_name: current_user.name, controller: controller_name }
+    { ip: request.remote_ip, whodunnit_name: current_user.try(:name), controller: controller_name }
   end
-  
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
