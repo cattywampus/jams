74a6468592cdb3b8b80a2465d17782e884cd66d5
Keith Walters <keith.walters@cattywamp.us>
2013-08-03 12:49:54 -0400

    Updated capistrano deployment recipes, new server

--- config/application.rb
+++ config/application.rb
@@ -73,5 +73,8 @@ module Jams
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # Speed things up by not loading Rails env
+    config.assets.initialize_on_precompile = false
   end
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,69 +1,28 @@
-require 'bundler/capistrano'
+require "bundler/capistrano"
 
-load 'config/recipes/rails.rb'
+load "config/recipes/base"
+load "config/recipes/nginx"
+load "config/recipes/unicorn"
+load "config/recipes/postgresql"
+load "config/recipes/nodejs"
+load "config/recipes/rbenv"
+load "config/recipes/app"
+load "config/recipes/check"
 
-set :address, "50.116.32.251"
+server "192.241.172.183", :web, :app, :db, primary: true
 
-server "#{address}", :web, :app, :db, primary: true
-
-set :application, "jams"
 set :user, "deployer"
+set :application, "jams"
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
+set :shared_children, shared_children + %w{public/uploads}
 
 set :scm, "git"
-set :repository, "ssh://#{user}@#{address}/git/#{application}.git"
+set :repository, "ssh://git@bitbucket.org/cattywampus/jams.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
-set :shared_children, shared_children + %w{public/uploads}
-
-after "deploy", "deploy:cleanup" # keep only the last 5 releases
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-namespace :deploy do
-  %w[start stop restart].each do |command|
-    desc "#{command.capitalize} unicorn server"
-    task command, roles: :app, except: {no_release: true} do
-      run "/etc/init.d/unicorn_#{application} #{command}"
-    end
-  end
-
-  task :setup_config, roles: :app do
-    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
-    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
-    run "mkdir -p #{shared_path}/config"
-    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
-    put File.read("config/application.example.yml"), "#{shared_path}/config/application.yml"
-    puts "Now edit the config files in #{shared_path}."
-  end
-  after "deploy:setup", "deploy:setup_config"
-
-  task :symlink_config, roles: :app do
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-    run "ln -nfs #{shared_path}/config/application.yml #{release_path}/config/application.yml"
-  end
-  after "deploy:finalize_update", "deploy:symlink_config"
-
-  desc "Make sure local git is in sync with remote."
-  task :check_revision, roles: :web do
-    unless `git rev-parse HEAD` == `git rev-parse origin/master`
-      puts "WARNING: HEAD is not the same as origin/master"
-      puts "Run `git push` to sync changes."
-      exit
-    end
-  end
-  before "deploy", "deploy:check_revision"
-end
-
-desc "install the necessary prerequisites"
-task :bundle_install, :roles => :app do
-  run "cd #{release_path} && bundle install"
-end
-after "deploy:update_code", :bundle_install
-
+after "deploy", "deploy:cleanup"
