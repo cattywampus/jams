31a909682b4998a2d90dd29242f1eaf2f66a6e71
Keith Walters <keith.walters@cattywamp.us>
2015-01-04 20:49:38 -0500

    Enum select now works with rails 4, lists values

--- app/inputs/enum_select_input.rb
+++ app/inputs/enum_select_input.rb
@@ -1,10 +1,29 @@
 class EnumSelectInput < SimpleForm::Inputs::CollectionSelectInput
-  def input
+  def input(wrapper_options = nil)
     label_method, value_method = detect_collection_methods
+    
+    merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
 
-    @builder.enum_select(
-        attribute_name,
-        input_options, input_html_options
-    )
+    options[:include_blank?] = enums.allows_nil? if options[:include_blank].nil?
+    
+    @builder.collection_select(
+           attribute_name, collection, value_method, label_method,
+           input_options, merged_input_options
+         )
   end
+  
+  private
+
+  def collection
+    @collection ||= begin
+      collection = enums ? enums.select_options : []
+    end
+  end
+  
+  def enums
+    if @builder.object.respond_to?(:enums)
+      @builder.object.enums(attribute_name)
+    end
+  end
+  
 end
